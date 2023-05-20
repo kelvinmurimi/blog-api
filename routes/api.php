00d2e1325fbcc708e0242dfd6d87ecd56222a0f0
kelvinmurimi <murimikelvin229@gmail.com>
2023-05-21 02:58:22 +0300

    finnished title & body fields crud

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\ap1\v1\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
+Route::group(['prefix'=>'v1'],function(){
+  Route::apiResource('posts',PostsController::class);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
